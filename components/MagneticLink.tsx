66baaa2d827737e4189796b2450009648bcef4d7
haibt163 <buithanhhaicpa@gmail.com>
2026-09-07 17:57:11 +0700

    Refine magnetic link typing and interaction

--- components/MagneticLink.tsx
+++ components/MagneticLink.tsx
@@ -1,16 +1,16 @@
 "use client";
 
-import Link, { type LinkProps } from "next/link";
-import { type MouseEvent, type PropsWithChildren } from "react";
+import Link from "next/link";
+import type { ComponentPropsWithoutRef, MouseEvent, PropsWithChildren } from "react";
 
-type MagneticLinkProps = PropsWithChildren<LinkProps & { className?: string }>;
+type MagneticLinkProps = PropsWithChildren<ComponentPropsWithoutRef<typeof Link>>;
 
 export function MagneticLink({ children, className = "", ...props }: MagneticLinkProps) {
   function handleMove(event: MouseEvent<HTMLAnchorElement>) {
     const target = event.currentTarget;
     const rect = target.getBoundingClientRect();
-    const x = ((event.clientX - rect.left) / rect.width - 0.5) * 8;
-    const y = ((event.clientY - rect.top) / rect.height - 0.5) * 8;
+    const x = ((event.clientX - rect.left) / rect.width - 0.5) * 10;
+    const y = ((event.clientY - rect.top) / rect.height - 0.5) * 10;
     target.style.setProperty("--magnetic-x", `${x}px`);
     target.style.setProperty("--magnetic-y", `${y}px`);
   }
